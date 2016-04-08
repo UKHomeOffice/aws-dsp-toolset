2e8d73879641a3fa06fa83de149364e6349ee98b
jon-shanks <jon.shanks@gmail.com>
2016-04-08 16:45:20 +0100

    adding in some logic

--- bin/encrypt_secrets.sh
+++ bin/encrypt_secrets.sh
@@ -5,6 +5,11 @@ set -o pipefail
 
 kms_master_key_id=$(cd stacks ; stacks config kms_master_key_id)
 
-s3secrets kms encrypt -k ${kms_master_key_id} stacks/config.d/secrets.yaml
-cat stacks/config.d/secrets.yaml.encrypted | base64 > stacks/config.d/secrets.yaml.enc && \
-  rm stacks/config.d/secrets.yaml.encrypted
+secrets_file_enc="stacks/config.d/secrets_${STACKS_ENV}.yaml.enc"
+secrets_tmp_enc="${secrets_file_enc}rypted"
+secrets_file="stacks/config.d/secrets_${STACKS_ENV}.yaml"
+
+if [[ -f "${secrets_file}" ]]; then
+  s3secrets kms encrypt -k ${kms_master_key_id} "${secrets_file}"
+  cat ${secrets_tmp_enc} | base64 > "${secrets_file_enc}" && rm ${secrets_tmp_enc}
+fi
